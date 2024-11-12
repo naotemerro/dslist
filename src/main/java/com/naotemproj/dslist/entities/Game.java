a8ce713837a608138baf547be1b864b8805f1311
Victor Marques <naotemerro@hotmail.com>
2024-11-11 22:20:17 -0300

    Game seed

--- src/main/java/com/naotemproj/dslist/entities/Game.java
+++ src/main/java/com/naotemproj/dslist/entities/Game.java
@@ -17,14 +17,15 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	
-	@Column (name = "game_year")
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {	
